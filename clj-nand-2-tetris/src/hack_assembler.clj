ba957fd8bf9c41214dbbf39f3658f6ac0cf22de9
Eloy Robillard <eloy.robillard@gmail.com>
2025-10-11 21:01:26 +0200

    Do symbol analysis path

--- clj-nand-2-tetris/src/hack_assembler.clj
+++ clj-nand-2-tetris/src/hack_assembler.clj
@@ -1,18 +1,15 @@
 (ns hack-assembler
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as str])
+  (:require [symbol-table :as st])
   (:require [code :as c])
   (:require [parser :as p]))
 
-(defn rm-non-code-aux [acc seq]
-  (let [head (first seq)]
-    (cond
-      (nil? head) acc
-      (or (p/is-ws? head) (p/is-cmt? head)) (recur acc (rest seq))
-      :else (recur (cons head acc) (rest seq)))))
-
 (defn rm-non-code [seq]
-  (reverse (rm-non-code-aux '() seq)))
+  (filter #(not (or (p/is-ws? %) (p/is-cmt? %))) seq))
+
+(defn rm-l-instr [seq]
+  (filter #(not (= (p/instruction-type %) :l-instr)) seq))
 
 ; (rm-non-code '())
 ; (rm-non-code '("" "// sdf" "sdf" "// ewr" ""))
@@ -24,23 +21,68 @@
         jmp (p/jump c-instr)]
     (str/join "" ["111" (c/cmp comp) (c/dest dest) (c/jump jmp)])))
 
-(defn assemble-aux [seq]
-  (when (some? (first seq))
-    (let [instr (first seq)
-          type (p/instruction-type instr)]
-      (if (or (= type :a-instr) (= type :l-instr))
-        (println (p/sym type instr))
-        (println (c-to-bits instr))))
-    (recur (p/advance seq))))
+(defn populate-st-aux [seq ln st]
+  {:pre [(map? st)]}
+  (if-not (some? (first seq))
+    st
+    (let [head (first seq)]
+      (if (= :l-instr (p/instruction-type head))
+        (let [sym (p/sym head :l-instr)]
+          (if (st/contains st sym)
+            (recur (rest seq) ln st)
+            (recur (rest seq) ln (st/add-entry st sym ln))))
+        (recur (rest seq) (+ ln 1) st)))))
+
+(defn populate-symbol-table [seq st]
+  (populate-st-aux seq 0 st))
+
+(defn str-to-bin [str]
+  (-> str
+      Integer/parseInt
+      (Integer/toString 2)
+      Integer/parseInt))
+
+; (instr-to-bin "@128")
+; (format "%016d" (instr-to-bin "@128"))
+
+(defn sym-to-bin [st sym]
+  {:pre [(map? st) (not (Character/isDigit (first sym)))]
+   :post [(int? %)]}
+  (-> sym
+      (#(st/get-address st %))
+      (Integer/toString 2)
+      Integer/parseInt))
+
+(defn assemble-aux [seq st]
+  {:pre [(map? st)]
+   :post [(every? string? %)]}
+  (map (fn [instr]
+         (let [type (p/instruction-type instr)]
+           (if (= type :a-instr)
+             (let [sym (p/sym instr :a-instr)]
+               (if (Character/isDigit (first sym))
+                 (format "%016d" (str-to-bin sym))
+                 (format "%016d" (sym-to-bin st sym))))
+             (c-to-bits instr))))
+       seq))
+
+(def st {"R0" 0, "R1" 1, "R2" 2, "R3" 3, "R4" 4, "R5" 5, "R6" 6, "R7" 7, "R8" 8, "R9" 9, "R10" 10, "R11" 11, "R12" 12, "R13" 13, "R14" 14, "R15" 15, "SP" 0, "LCL" 1, "ARG" 2, "THIS" 3, "THAT" 4, "SCREEN" 16384, "KBD" 24576})
 
 (defn assemble [seq]
-  (let [sq (rm-non-code seq)]
-    (assemble-aux sq)))
+  (let [st (populate-symbol-table seq st)]
+    (let [res (-> seq
+                  rm-non-code
+                  rm-l-instr
+                  (assemble-aux st))]
+      (println st)
+      res)))
 
 ; (assemble '("" "// swer" "@456" "@123" "(echo)" "D=0" "0;JMP"))
 ; (assemble '("@456" "@123" "" "// wer" "(echo)" "D=0" "0;JMP"))
+; (assemble '("(ONE)" "@ONE" "@123" "" "// wer" "(echo)" "D=0" "0;JMP"))
+; (run! println (assemble '("(ONE)" "@ONE" "@123" "" "// wer" "(echo)" "D=0" "0;JMP")))
 
-(let [file (with-open [rdr (clojure.java.io/reader (first *command-line-args*))]
-             (into [] (line-seq rdr)))] (assemble file))
-
-
+(let [fin (with-open [rdr (clojure.java.io/reader (first *command-line-args*))]
+            (into [] (line-seq rdr)))]
+  (spit "test.hack" (str/join "\n" (assemble fin))))
+    ; (run! (.write w) (map str (assemble fin)))))
--- clj-nand-2-tetris/src/parser.clj
+++ clj-nand-2-tetris/src/parser.clj
@@ -1,7 +1,7 @@
 (ns parser
+  (:require [symbol-table :as st])
+  (:require [clojure.pprint :as pp])
   (:require [clojure.string :as str]))
-
-(defn has-more-lines [seq] (some? (first (rest seq))))
 
 (defn is-a? [instr] (str/starts-with? (str/triml instr) "@"))
 (defn is-l? [instr]
@@ -11,38 +11,10 @@
 (defn is-cmt? [instr] (str/starts-with? (str/triml instr) "//"))
 (defn is-ws? [instr] (empty? (str/triml instr)))
 
-(defn skip-non-code [seq]
-  (loop [sq seq] (if-not (and
-                          (has-more-lines sq)
-                          (or
-                           (is-ws? (first sq))
-                           (is-cmt? (first sq))))
-                   sq
-                   (recur (rest sq)))))
-
 ; (has-more-lines '("" "// sdf"))
 ; (is-cmt? "  // sdf")
 ; (is-ws? "")
 ; (is-cmt? "  // sdf")
-; (skip-non-code '("" "// sdf" "sdfsdf" "// sdf"))
-; (skip-non-code '("// sdf" "" "sdfsdf" "// sdf"))
-; (skip-non-code '("// sdf" "sdf"))
-; (skip-non-code '("sdf" "// sdf"))
-; (skip-non-code '(""))
-; (skip-non-code '())
-
-(defn advance [seq]
-  (let [sq (skip-non-code seq)]
-    (if (some? (first sq))
-      (rest sq)
-      sq)))
-
-; (advance '("" "// sdf" "sdfsdf" "// sdf"))
-; (advance '("// sdf" "" "sdfsdf" "// sdf"))
-; (advance '("// sdf" "sdf"))
-; (advance '("sdf" "// sdf"))
-; (advance '(""))
-; (advance '())
 
 (defn instruction-type [instr]
   (cond
@@ -50,15 +22,17 @@
     (is-l? instr) :l-instr
     :else :c-instr))
 
-(defn sym [type instr]
-  (cond
-    (= type :a-instr) (str/replace instr "@" "")
-    (= type :l-instr) (str/replace instr #"[()]" "")
-    :else instr))
+(defn sym [instr type]
+  {:pre [(or (= type :a-instr) (= type :l-instr))]
+   :post [(some? %)]}
+  (if (= type :a-instr)
+    (str/replace (str/trim instr) "@" "")
+    (str/replace (str/trim instr) #"[()]" "")))
 
-; (sym :a-instr "@123")
-; (sym :a-instr "@wer")
-; (sym :l-instr "(LOOP)")
+; (sym "@123" {})
+; (sym "@THIS" {"THIS" 3})
+; (st/add-entry {} "NIL" 100)
+; (sym "@NIL" {})
 
 (defn dest [instr]
   (let [[_ match] (re-find #"(\w+)=" instr)]
