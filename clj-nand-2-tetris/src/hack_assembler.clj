304f80fbc9ad4e86b91135954e1b93df154c8f40
Eloy Robillard <eloy.robillard@gmail.com>
2025-10-13 16:55:03 +0200

    Complete symbol table paths

--- clj-nand-2-tetris/src/hack_assembler.clj
+++ clj-nand-2-tetris/src/hack_assembler.clj
@@ -21,37 +21,60 @@
         jmp (p/jump c-instr)]
     (str/join "" ["111" (c/cmp comp) (c/dest dest) (c/jump jmp)])))
 
-(defn populate-st-aux [seq ln st]
+(defn populate-st-l [st seq ln]
   {:pre [(map? st)]}
   (if-not (some? (first seq))
     st
-    (let [head (first seq)]
-      (if (= :l-instr (p/instruction-type head))
+    (let [head (first seq)
+          type (p/instruction-type head)]
+      (if (= :l-instr type)
         (let [sym (p/sym head :l-instr)]
           (if (st/contains st sym)
-            (recur (rest seq) ln st)
-            (recur (rest seq) ln (st/add-entry st sym ln))))
-        (recur (rest seq) (+ ln 1) st)))))
+            (recur st (rest seq) ln)
+            (recur (st/add-entry st sym ln) (rest seq) ln)))
+        (recur st (rest seq) (+ ln 1))))))
+
+(defn populate-st-a [st seq addr]
+  {:pre [(>= addr 16) (< addr 16384) (map? st)]}
+  (if-not (some? (first seq))
+    st
+    (let [head (first seq)
+          type (p/instruction-type head)]
+      (if (= :a-instr type)
+        (let [sym (p/sym head :a-instr)]
+          (if (or (st/contains st sym) (Character/isDigit (first sym)))
+            (recur st (rest seq) addr)
+            (recur (st/add-entry st sym addr) (rest seq) (+ addr 1))))
+        (recur st (rest seq) addr)))))
 
 (defn populate-symbol-table [seq st]
-  (populate-st-aux seq 0 st))
+  {:pre [(map? st)]}
+  (-> st
+      (populate-st-l seq 0)
+      (populate-st-a seq 16)))
+
+(defn pad-left [s c len]
+  (let [slen (.length s)]
+    (.concat
+     (.repeat c (- len slen))
+     s)))
+
+; (pad-left "123" "0" 8)
 
 (defn str-to-bin [str]
   (-> str
-      Integer/parseInt
-      (Integer/toString 2)
-      Integer/parseInt))
+      Integer/parseUnsignedInt
+      (Integer/toString 2)))
 
 ; (instr-to-bin "@128")
 ; (format "%016d" (instr-to-bin "@128"))
 
 (defn sym-to-bin [st sym]
   {:pre [(map? st) (not (Character/isDigit (first sym)))]
-   :post [(int? %)]}
+   :post [(string? %)]}
   (-> sym
       (#(st/get-address st %))
-      (Integer/toString 2)
-      Integer/parseInt))
+      (Integer/toString 2)))
 
 (defn assemble-aux [seq st]
   {:pre [(map? st)]
@@ -61,24 +84,23 @@
            (if (= type :a-instr)
              (let [sym (p/sym instr :a-instr)]
                (if (Character/isDigit (first sym))
-                 (format "%016d" (str-to-bin sym))
-                 (format "%016d" (sym-to-bin st sym))))
+                 (pad-left (str-to-bin sym) "0" 16)
+                 (pad-left (sym-to-bin st sym) "0" 16)))
              (c-to-bits instr))))
        seq))
 
 (def st {"R0" 0, "R1" 1, "R2" 2, "R3" 3, "R4" 4, "R5" 5, "R6" 6, "R7" 7, "R8" 8, "R9" 9, "R10" 10, "R11" 11, "R12" 12, "R13" 13, "R14" 14, "R15" 15, "SP" 0, "LCL" 1, "ARG" 2, "THIS" 3, "THAT" 4, "SCREEN" 16384, "KBD" 24576})
 
 (defn assemble [seq]
-  (let [st (populate-symbol-table seq st)]
-    (-> seq
-        rm-non-code
+  (let [sq (rm-non-code seq)
+        st (populate-symbol-table sq st)]
+    (-> sq
         rm-l-instr
         (assemble-aux st))))
 
 ; (assemble '("" "// swer" "@456" "@123" "(echo)" "D=0" "0;JMP"))
 ; (assemble '("@456" "@123" "" "// wer" "(echo)" "D=0" "0;JMP"))
 ; (assemble '("(ONE)" "@ONE" "@123" "" "// wer" "(echo)" "D=0" "0;JMP"))
-; (run! println (assemble '("(ONE)" "@ONE" "@123" "" "// wer" "(echo)" "D=0" "0;JMP")))
 
 (let [fin (with-open [rdr (clojure.java.io/reader (first *command-line-args*))]
             (into [] (line-seq rdr)))]
