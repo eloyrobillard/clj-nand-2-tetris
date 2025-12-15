76e7d1615599b1b57b9bf98fe9e0948ce4c465ee
Eloy Robillard <eloy.robillard@gmail.com>
2025-12-15 10:48:55 +0100

    Move files to interpreter/

---
+++ interpreter/symbol_table.clj
@@ -0,0 +1,12 @@
+(ns symbol-table)
+
+(defn add-entry [st key val]
+  {:pre [(map? st) (string? key) (int? val)]}
+  (assoc st key val))
+
+(defn contains [st key]
+  (some? (st key)))
+
+(defn get-address [st key]
+  {:post [(int? %)]}
+  (st key))
