9d2736b52cd3b6c46fe62634865d84d475696f90
Eloy Robillard <eloy.robillard@gmail.com>
2026-01-13 15:45:47 +0100

    Simplify fn test

--- src/vm/test/vm.clj
+++ src/vm/test/vm.clj
@@ -1,9 +1,12 @@
 (ns vm.test.vm
   (:require [vm]
+            [clojure.string :as str]
             [vm.parser :as p]
             [vm.code-writer :as cw]
             [hack-interpreter :as itp]))
 
-(def fn-call-test ["function Foo 0" "call Bar 0" "return" "function Bar 0" "push constant 85" "return" "function Sys.init 0" "call Foo 0" "label LOOP" "goto LOOP"])
+(def fn-call-test ["function Foo 0" "return" "function Sys.init 0" "call Foo 0" "label LOOP" "goto LOOP"])
 
-(itp/interpret (vm/vm-to-asm "Filename" fn-call-test))
+(def asm (vm/vm-to-asm "Filename" fn-call-test))
+(println (str/join "\n" asm))
+(itp/interpret asm)
