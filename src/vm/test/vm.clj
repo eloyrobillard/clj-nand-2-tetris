2b474a8376b4af48348d9d100a7b1e37d12cf198
Eloy Robillard <eloy.robillard@gmail.com>
2026-01-13 16:51:46 +0100

    Test top-of-stack value

--- src/vm/test/vm.clj
+++ src/vm/test/vm.clj
@@ -5,7 +5,7 @@
             [vm.code-writer :as cw]
             [hack-interpreter :as itp]))
 
-(def fn-call-test ["function Foo 0" "return" "function Sys.init 0" "call Foo 0" "label LOOP" "goto LOOP"])
+(def fn-call-test ["function Foo 0" "call Bar 0" "return" "function Bar 0" "push constant 85" "return" "function Sys.init 0" "call Foo 0" "label LOOP" "goto LOOP"])
 
 (def asm (vm/vm-to-asm "Filename" fn-call-test))
 (println (str/join "\n" asm))
